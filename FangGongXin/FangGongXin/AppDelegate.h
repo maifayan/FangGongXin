cd8eef0731b122b44e62d34c8fefeecc27b033f7
麦法艳 <987872609@qq.com>
2017-09-19 11:06:46 +0800

    创建工程

---
+++ FangGongXin/FangGongXin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FangGongXin
+//
+//  Created by Apple on 2017/9/19.
+//  Copyright © 2017年 YaLeTeCh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
